36de9e331a97ae31380b9915c5aee7b2e67262f9
George Hawkins <george-hawkins@users.noreply.github.com>
2021-05-11 22:19:31 +0200

    TemporaryFilePath defaults to /tmp anyway - no need to set to something else.

--- nvidia-setup.sh
+++ nvidia-setup.sh
@@ -5,7 +5,7 @@
 # Then run this script - it's constructed from the README in that directory.
 # Running the script isn't enough - things only started working once I rebooted the system.
 
-echo 'options nvidia NVreg_PreserveVideoMemoryAllocations=1 NVreg_TemporaryFilePath=/var/tmp' | sudo tee /etc/modprobe.d/nvidia-power-management.conf > /dev/null
+echo 'options nvidia NVreg_PreserveVideoMemoryAllocations=1' | sudo tee /etc/modprobe.d/nvidia-power-management.conf > /dev/null
 
 cd /usr/share/doc/nvidia-driver-460
 
@@ -17,4 +17,4 @@ sudo cp nvidia-sleep.sh /usr/bin
 
 sudo systemctl enable nvidia-suspend.service
 sudo systemctl enable nvidia-hibernate.service
-sudo systemctl enable nvidia-resume.service
+#sudo systemctl enable nvidia-resume.service
